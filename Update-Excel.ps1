439fdcac5eba3fa8518f4552df984484885575d4
orrwil <orrwil@gmail.com>
2014-07-08 23:15:15 +0100

    Initial File creation
    initial parts of script

---
+++ Update-Excel.ps1
@@ -0,0 +1,15 @@
+﻿
+
+## Create a spreadsheet with test data
+
+$excel = New-Object -ComObject Excel.Application
+$excel.visible = $true
+$workbook = $excel.Workbooks.Add()
+$excel.Cells.item(1,1) = "Cell 1,1"
+$workbook.SaveAs("C:\Scripts\Test.xlsx")
+$excel.Quit()
+
+## Reoprn newly created sheet
+$excel = New-Object -ComObject Excel.Application
+$excel.visible = $true
+$workbook = $excel.Workbooks.Open("C:\Scripts\Test.xlsx")
